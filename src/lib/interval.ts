b04b1a7d350d3be6f9823c4f97332e63c5585f69
Guilherme Santos <guilherme.santos@doclerholding.com>
2021-03-29 11:53:18 +0200

    exercise-3 completed

--- src/lib/interval.ts
+++ src/lib/interval.ts
@@ -1,3 +1,25 @@
+import {IObserver, IProducer} from "../types";
+import {Observable} from "./observable";
+
 export function interval(period: number) {
-  throw new Error("Should be implemented");
+  const producer: IProducer = (observer: IObserver) => {
+    let counter = 0;
+
+    const id = setInterval(() => {
+      observer.next(counter++);
+    }, period);
+
+
+    if (typeof observer.complete === 'function') {
+      observer.complete();
+    }
+
+    return {
+      unsubscribe: () => {
+        clearInterval(id);
+      }
+    };
+  };
+
+  return new Observable(producer);
 }
